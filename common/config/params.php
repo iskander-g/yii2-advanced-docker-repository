504e2c6ba528a5a68a2869c9308d8ab3e2e0c634
Developer <dev@backend>
2023-08-10 11:22:52 +0300

    fixes

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'adminEmail' => 'admin@rt-express.ru',
-    'supportEmail' => 'admin@rt-express.ru',
-    'senderEmail' => 'admin@rt-express.ru',
-    'senderName' => 'RT-Express.ru mailer',
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'admin@example.com',
+    'senderEmail' => 'admin@example.com',
+    'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
 ];
